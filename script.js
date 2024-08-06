8f0a3220f0964c0a5f6a44de0f8721198219ca9c
huseyineskan01@gmail.com <huseyineskan01@gmail.com>
2024-08-06 22:08:30 +0200

    added js

--- script.js
+++ script.js
@@ -6,7 +6,15 @@ const median = nums => {
   const sorted = nums.slice().sort((a, b) => a - b);
   const length = sorted.length;
   const middle = length / 2 - 1;
+  return isEven(length)
+    ? average([sorted[middle], sorted[middle + 1]])
+    : sorted[Math.ceil(middle)];
+}
 
+const spreadsheetFunctions = {
+  sum,
+  average,
+  median
 }
 
 const range = (start, end) => Array(end - start + 1).fill(start).map((element, index) => element + index);
@@ -29,7 +37,12 @@ window.onload = () => {
       input.type = "text";
       input.id = letter + number;
       input.ariaLabel = letter + number;
+
       container.appendChild(input);
     })
   })
+}
+
+const update = event => {
+
 }
